7dff2a849df738de1f7a38464d6ed8bd75a4b640
Lukaku55 <Garciamarinolucas0@gmail.com>
2026-05-05 09:56:42 -0300

    fix: eliminar redirect loop en next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/blog",
-        destination: "/blog",
-        permanent: false,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
