107bd9d7a95d1142c25b81cea301630c3c0dceb4
Lukaku55 <Garciamarinolucas0@gmail.com>
2026-06-01 09:30:15 -0300

    Extract scroll-to-hash into a tiny client component

---
+++ components/ScrollToHash.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useEffect } from "react";
+
+export default function ScrollToHash() {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      setTimeout(() => {
+        const el = document.querySelector(hash);
+        if (el) el.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  }, []);
+  return null;
+}
